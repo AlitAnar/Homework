e2bca5aeed514cc92854ab2b833efb8737318d14
AlitAnar <shohrux_ibrohimov@mail.ru>
2021-11-10 22:56:26 +0200

    Add file

---
+++ main2.py
@@ -0,0 +1,8 @@
+num = int(input("Введите свой номер: "))
+if num <= 1:
+    print(num, "процент")
+elif num >= 2 and num <= 4:
+    print(num, "процента")
+else:
+    num >= 5 and num <= 100
+    print(num, "процентов")
