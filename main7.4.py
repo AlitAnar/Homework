d42e357667805653d7f6bdf81a3cc6cca7fb7cc3
AlitAnar <shohrux_ibrohimov@mail.ru>
2021-12-06 19:23:43 +0200

    Add file 7

--- main7.4.py
+++ main7.4.py
@@ -7,7 +7,7 @@ result = {0: 0, 10: 0, 100: 0, 1000: 0, 10000: 0, 100000: 0, 1000000: 0}
 keys = [0, 10, 100, 1000, 10000, 100000, 1000000]
 for root, dirs, files in os.walk(data_path):
     for _file in files:
-        sz = os.stst(os.path.join(root, _file)).st_size
+        sz = os.stat(os.path.join(root, _file)).st_size
         if sz ==0:
             result[0] += 1
             continue
